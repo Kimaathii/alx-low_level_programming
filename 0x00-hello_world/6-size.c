2c72b862c387ef9d9f2a0b7d6172c8a3c23f24c4
kimaathii <kimkimathi34@gmail.com>
2023-06-15 19:16:18 -0700

    use size of in c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,15 +7,22 @@
 int main(void)
 {
 
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float e;
-        printf("Size of a char: %zu byte\n",sizeof(a));
-	printf("Size of a int: %zu byte\n",sizeof(b));
-	printf("Size of a long int: %zu byte\n",sizeof(c));
-	printf("Size of a long long int: %zu byte\n",sizeof(d));
-	printf("Size of a float: %zu byte\n",sizeof(e));
-        return (0);
+
+char a;
+
+int b;
+
+long int c;
+
+long long int d;
+
+float e;
+
+printf("Size of a char: %zu byte\n", sizeof(a));
+printf("Size of a int: %zu byte\n", sizeof(b));
+printf("Size of a long int: %zu byte\n", sizeof(c));
+printf("Size of a long long int: %zu byte\n", sizeof(d));
+printf("Size of a float: %zu byte\n", sizeof(e));
+
+return (0);
 }
