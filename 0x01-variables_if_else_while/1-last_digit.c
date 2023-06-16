545b3ef4c7aec07d60e16740106029560d7e29d1
kimaathii <kimkimathi34@gmail.com>
2023-06-16 10:55:26 -0700

    create a constructure with if statement

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,23 +10,25 @@
 int main(void)
 {
 	int n;
+	int last_digit;
+
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
+	last_digit = abs(n) % 10;
 	/* your code goes there */
 
 	if (n > 5)
 	{
-		printf("Last digit of n is %d and is greater than 5\n", n);
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 	};
 	if (n == 0)
 	{
-		printf("Last digit of n is %d and is 0\n", n);
+		printf("Last digit of %d is %d and is 0\n", n, last_digit);
 	};
 	if ((n < 6) && !(n == 0))
 	{
-		printf("Last digit of n is %d and is less than 6 and not 0\n", n);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
 	};
 	return (0);
 }
