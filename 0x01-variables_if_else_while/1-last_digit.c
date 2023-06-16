35fdeb931c9381de87fe5d32886161f06891fc0c
kimaathii <kimkimathi34@gmail.com>
2023-06-16 10:37:51 -0700

    create a constructure with if statement

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,6 +16,17 @@ int main(void)
 
 	/* your code goes there */
 
-	printf("Last digit of %d\n", n);
+	if (n > 5)
+	{
+		printf("Last digit of n is %d and is greater than 5\n", n);
+	};
+	if (n == 0)
+	{
+		printf("Last digit of n is %d and is 0\n", n);
+	};
+	if ((n < 6) && !(n == 0))
+	{
+		printf("Last digit of n is %d and is less than 6 and not 0\n", n);
+	};
 	return (0);
 }
