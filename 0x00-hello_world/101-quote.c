5f26c903628a82b39e57e5383d4443778321a38c
kimaathii <kimkimathi34@gmail.com>
2023-06-16 12:57:02 -0700

    fix bugs

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,8 +3,9 @@
  * main - entry point
  * Return: 1 Always(succeed)
  */
-int main() {
-    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-    return 1;
+int maiin(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
 
