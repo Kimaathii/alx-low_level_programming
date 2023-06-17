39f9026b8fe5b008850c8955ee93522127a8209a
kimaathii <kimkimathi34@gmail.com>
2023-06-17 06:42:48 -0700

    correct the code to output correct result

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,7 +5,7 @@
 /**
  * main - entry point, Prints the last digit of a random number
  *and if else condition to check whether
-  it is greater than 5, less than 6, or 0.
+ * it is greater than 5, less than 6, or 0.
  *
  * Return: Always 0.
  */
