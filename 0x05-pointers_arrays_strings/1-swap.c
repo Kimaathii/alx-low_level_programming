b89151d00f468dc49e39523e5d291ecc6f8d3c9f
kimaathii <kimkimathi34@gmail.com>
2023-06-26 13:21:23 -0700

    swap the address of char variables

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * swap_int - Swaps the values of two character.
- * @x: The first character to be swapped.
- * @y: The second character to be swapped.
+ * swap_int - Swaps the values of two integers.
+ * @a: The first integer to be swapped.
+ * @b: The second integer to be swapped.
  */
-void swap_int(char *x, char *y)
+void swap_int(int *a, int *b)
 {
-	char tmp = *x;
-	*x = *y;
-	*y = tmp;
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
